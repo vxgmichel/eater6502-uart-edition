dc9f462530359f4df4ea960e5893bd5548ea919b
Vincent Michel <vxgmichel@gmail.com>
2022-01-01 13:53:17 +0100

    Add typing program

---
+++ typing.asm
@@ -0,0 +1,51 @@
+; A program that receives data from the UART and shows it on the LCD display
+; It supports backspace and newline
+
+#include "map/subeater.asm"
+
+
+; Main program
+#bank program
+
+reset:
+  ; Init LCD
+  jsr lcd_init
+  jsr lcd_clear
+
+  ; Blinking cursor
+  lda #0b00001101
+  jsr lcd_instruction
+
+  ; Configure UART
+  jsr uart_init
+
+  .main:
+  jsr uart_read_one
+  cmp #0x7f
+  beq .del_char
+  cmp #"\n"
+  beq .new_line
+  .print_char:
+  jsr lcd_print_char
+  jmp .done
+  .del_char:
+  jsr lcd_del_char
+  jmp .done
+  .new_line:
+  jsr lcd_new_line
+  jmp .done
+
+  ; Loop forever
+  .done:
+  jmp .main
+
+; Interrupt
+
+nmi:
+irq:
+  rti
+
+; Libraries
+
+#include "lib/lcd.asm"
+#include "lib/uart.asm"
---
+++ typing.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+Send raw unbuffer data from stdin to ttyUSB0
+"""
+
+import sys
+import tty
+import contextlib
+import subprocess
+
+@contextlib.contextmanager
+def raw_stdin(stdin=None):
+    # Get stdin file descriptor
+    if stdin is None:
+        stdin = sys.stdin
+    fd = stdin.fileno()
+
+    # Backup the current TTY configuration
+    old_tty = tty.tcgetattr(fd)
+    try:
+        # Set stdin to raw and cbreak
+        tty.setraw(fd)
+        tty.setcbreak(fd)
+
+        # Set stdin to non-blocking mode
+        yield stdin
+
+    # Restore stdin original configuration
+    finally:
+        tty.tcsetattr(fd, tty.TCSADRAIN, old_tty)
+
+
+def main():
+    subprocess.run("""\
+stty -F /dev/ttyUSB0 \
+  speed 4800 \
+  cs8 cstopb parenb -parodd -cmspar \
+  -hupcl clocal cread crtscts \
+  -ocrnl -ofdel -ofill -olcuc -onlcr -onlret -onocr -opost \
+  raw""", shell=True, check=True, capture_output=True)
+    with raw_stdin():
+        with open("/dev/ttyUSB0", "wb", buffering=0) as serial:
+            while True:
+                char = sys.stdin.buffer.read(1)
+                if char in b'\x03\x04':
+                    break
+                serial.write(char)
+
+
+if __name__ == "__main__":
+    main()
