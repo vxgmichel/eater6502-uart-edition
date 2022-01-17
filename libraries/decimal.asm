c4dc85602e8a9e463d7b02a880c492b89bb5cd7a
Vincent Michel <vxgmichel@gmail.com>
2022-01-17 14:14:58 +0100

    Refactor decimal library

--- libraries/decimal.asm
+++ libraries/decimal.asm
@@ -14,69 +14,56 @@ division_buffer: #res 2 * 16
 ; Parse the null-terminated string provided at address (a0-a1)
 ; And return the corresponding value in a0-a1
 from_base10:
-  tya
-  pha
-  lda r0
-  pha
+  tya            ; Push Y
+  pha            ; Onto the stack
 
-  ldy #0x00
-  lda #0x00
-  sta s0
-  sta s1
-  sta s2
-  sta s3
-  sta r0
+  ldy #0         ; Set Y to zero
+  wrw #0 s0      ; Set s0 word to zero
+  wrw #0 s2      ; Set s1 word to zero
+  wrb #0 s4      ; Set s4 to zero
 
-  .char_loop:
-  lda (a0), y
-  sec
-  sbc #"0"
-  bmi .done
-  sta r0
+  .char_loop:    ; Loop over characters
+  lda (a0), y    ; Get character
+  sec            ; Prepare subtraction
+  sbc #"0"       ; Convert to integer
+  bmi .done      ; Not a digit, we're done
+  sta s4         ; Store integer in s4
 
-  clc
-  asl s0 ; Double s0-s1
-  rol s1 ; ...
+  asl s0         ; Double s0 word
+  rol s1         ; Propagate carry
 
-  lda s0 ; Copy to s2-s3
-  sta s2 ; ...
-  lda s1 ; ...
-  sta s3 ; ...
+  wrw s0 s2      ; Copy s0 word to s2
 
-  asl s0 ; Double s0-s1
-  rol s1 ; ...
+  asl s0         ; Double s0 word
+  rol s1         ; Propagate carry
 
-  asl s0 ; Double s0-s1
-  rol s1 ; ...
+  asl s0         ; Double s0 word
+  rol s1         ; Propagate carry
 
-  lda s0 ; Add s2-s3 to s0-s1
-  adc s2 ; Effectively multiplying by 10
-  sta s0
-  lda s1
-  adc s3
-  sta s1
+  clc            ; Prepare addition
+  lda s0         ; Add s2 word to s0
+  adc s2         ; Effectively multiplying by 10
+  sta s0         ; ...
+  lda s1         ; ...
+  adc s3         ; ...
+  sta s1         ; ...
 
-  lda s0 ; Add new digit
-  adc r0 ; and propagate carry
-  sta s0
-  lda s1
-  adc #0x00
-  sta s1
+  clc            ; Prepare addition
+  lda s0         ; Add new digit
+  adc s4         ; and propagate carry
+  sta s0         ; ...
+  lda s1         ; ...
+  adc #0         ; ...
+  sta s1         ; ...
 
-  iny
-  jmp .char_loop
+  iny            ; Increment Y
+  jmp .char_loop ; Loop over
 
   .done:
-  lda s0 ; Copy s0-s1 to a0-a1
-  sta a0 ; ...
-  lda s1 ; ...
-  sta a1 ; ...
-
-  pla
-  sta r0
-  pla
-  tay
-  rts
+  wrw s0 a0      ; Write s0 word to a0
+  pla            ; Pull Y
+  tay            ; From the stack
+  rts            ; Return from subroutine
 
 
 ; Convert a number to ascii string in base 10
@@ -85,105 +72,11 @@ from_base10:
 ; - a2-a3: buffer to write
 ; - a4-a5: end of buffer
 to_base10:
-  txa           ; Push X-Y on the stack
-  pha
-  tya
-  pha
-
-  lda r0        ; Push r0-r1 onto the stack
-  pha
-  lda r1
-  pha
-
-  lda a2        ; Store a2-a3 to r0-r1
-  sta r0
-  lda a3
-  sta r1
-
-  lda #0x0a                   ; Load 10 in a2-a3
-  sta a2
-  lda #0x00
-  sta a3
-
-  lda #division_buffer`8      ; Load division buffer to a4-a5
-  sta a4
-  lda #(division_buffer>>8)`8
-  sta a5
-
-  jsr successive_division     ; Perform successive division
-
-
-  ldx #0x00                   ; Loop over characters
-  lda a6                      ; Load result buffer size to Y
-  tay
-  .char_loop:
-  beq .done                   ; Loop over division buffer in reverse
-  dey                         ; Stop when when start of buffer is reached
-  dey
-
-  lda (a4), y                 ; Get the corresponding character
-  clc
-  adc #"0"
-  sta s1
-
-  txa                         ; Swap X-Y
-  sta s0
-  tya
-  tax
-  lda s0
-  tay
-
-  lda s1
-  sta (r0), y                 ; Write to output buffer
-  iny
-
-  txa                         ; Swap X-Y
-  sta s0
-  tya
-  tax
-  lda s0
-  tay
-
-  jmp .char_loop              ; Loop over characters
-
-  .done:
-
-  txa                         ; Transfer X to s0
-  sta s0
-
-  bne .skip                   ; Add a "0" if necessary
-  tay
-  lda #"0"
-  sta (r0), y
-  inc s0
-  .skip:
-
-  clc                         ; Copy buffer start to a2-a3
-  lda r0                      ; and buffer stop to a4-a5
-  sta a2
-  adc s0
-  sta a4
-  lda r1
-  sta a3
-  adc #0
-  sta a5
-
-  ldy #0x00                   ; Write a null byte at the end
-  lda #"\0"
-  sta (a4), y
-
-  pla                         ; Pull r0-r1 from the stack
-  sta r1
-  pla
-  sta r0
-
-  pla                         ; Pull X and Y from the stack
-  tay
-  pla
-  tax
-
-  rts                         ; Return
-
+  pha                        ; Save A on the stack
+  lda #1                     ; Zero-pagging of length 1
+  jsr to_base10_zero_padding ; Call conversion routine
+  pla                        ; Restore A register
+  rts                        ; Return from subroutine
 
 
 ; Convert a number to ascii string in base 10
@@ -198,7 +91,6 @@ to_base10_zero_padding:
   pha                     ; Onto the stack
   tya                     ; Push Y
   pha                     ; Onto the stack
-
   lda r0                  ; Push r0
   pha                     ; Onto the stack
   lda r1                  ; Push r1
@@ -224,8 +116,9 @@ to_base10_zero_padding:
   lda r2                  ; Load write length
   sec                     ; Prepare subtraction
   sbc s0                  ; Subtract buffer size
-  tay                     ; Transfer to Y
+  bmi .padding_end        ; No padding if negative
 
+  tay                     ; Transfer to Y
   .padding_loop:
   beq .padding_end        ; Done with padding
   lda #"0"                ; Load zero char
@@ -252,7 +145,7 @@ to_base10_zero_padding:
 
   wrw r0 a4               ; Write end of buffer to a4
   lda #"\0"               ; Write a null byte
-  sta (a4)                ; at the end of the buffer
+  sta (a4, x)             ; at the end of the buffer
 
   pla                     ; Pull r2
   sta r2                  ; From the stack
